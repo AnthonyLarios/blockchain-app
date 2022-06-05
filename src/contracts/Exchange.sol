bc04f63c640fe079463e787151e35b85791e7377
Anthony <simpsonsruleA52@gmail.com>
2022-06-05 00:02:29 -0700

    Add withdrawToken function to Exchange.sol
    Add test for withdrawToken in Exchange.test.js

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -41,7 +41,7 @@ contract Exchange {
 
   function withdrawEther(uint256 _amount) public {
     require(tokens[ETHER][msg.sender] >= _amount);
-    
+
     tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
     msg.sender.transfer(_amount);
     emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
@@ -53,5 +53,11 @@ contract Exchange {
 
     tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
     emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+  }
+
+  function withdrawToken(address _token, uint256 _amount) public {
+    require(Token(_token).transfer(msg.sender, _amount));
+    
+    tokens[_token][msg.sender] = tokens[_token][msg.sender].sub(_amount);
   }
 }
